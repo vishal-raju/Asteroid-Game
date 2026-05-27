57136b3f49ba2c7c976ee0fc0eee61d8ae83b079
vishal <vishalfgm123@gmail.com>
2026-05-27 10:44:09 +0530

    initialised sprites

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
 
         log_state()
@@ -23,7 +26,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-
+        dt = clock.tick(60)/1000
+        
 
 
 
